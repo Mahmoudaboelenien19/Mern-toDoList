a75555a507a768afd0b64793dd547689815eb6f6
unknown <103467245+Mahmoudaboelenien19@users.noreply.github.com>
2023-01-29 04:22:11 +0200

    first submit add  sass and ready to start coding

---
+++ react/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./styles/main.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>mahmoud</p>
+    </div>
+  );
+};
+
+export default App;
